1354dff9c0a89a2d716a8114d8249ad7f8488ec7
Rosnel Alejandro Leyva-Cortes <30304057+Rosnel14@users.noreply.github.com>
2021-01-05 23:25:42 -0500

    Initial Commit

---
+++ Loot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loot
+//
+//  Created by Rosnel Leyva-Cortés on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
